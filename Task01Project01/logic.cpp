de0ce1732e3d5e022fcb145328ed9a41035522a3
vanikk54 <skugoli@gmail.com>
2025-03-16 19:42:46 +0300

    Update

--- Task01Project01/logic.cpp
+++ Task01Project01/logic.cpp
@@ -6,16 +6,21 @@ void  bubble_sort(int* array, int size, bool type) {
 	int count = 0;
 	for (int j = 0; j < size - 1; j++)
 	{
+		bool flag = true;
+
 		for (int i = 0; i < size - 1; i++)
 		{
 			count++;
 			if (type ? array[i] > array[i + 1] : array[1] < array[i + 1]) {
+				flag = false;
 				int t = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = t;
 			}
 		}
-	}
 
-	cout << "Count: " << count << endl;
+		if (flag) {
+			break;
+		}
+	}
 }
--- Task01Project01/main.cpp
+++ Task01Project01/main.cpp
@@ -3,13 +3,13 @@
 #define SIZE 10
 
 int main() {
-	int array[SIZE]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+	int array[SIZE]; // {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
-	//init(array, SIZE, -50, 50);
+	init(array, SIZE, -50, 50);
 	cout << "Array before sorting: " << convert(array, SIZE) << endl;
 
-	bubble_sort(array, SIZE, true);
-	cout << "Array after sorting: " << convert(array, SIZE) << endl;
+	//bubble_sort(array, SIZE, true);
+	//cout << "Array after sorting: " << convert(array, SIZE) << endl;
 	
 	bubble_sort(array, SIZE, false);
 	cout << "Array after sorting: " << convert(array, SIZE) << endl;
